9eacf5f3e2961a3148d83c24be2c20b6bd84f74a
Phong <118888564+tapo131@users.noreply.github.com>
2023-06-27 17:57:51 +0700

    Add files via upload

--- agents/AlphaBetaPruningAgent.py
+++ agents/AlphaBetaPruningAgent.py
@@ -22,13 +22,14 @@ class AlphaBetaPruningAgent(ABCAgent):
         score += self.territory_score(state)  # Đánh giá lãnh thổ
         # print('***')
         # print(score)
-        score -= self.attack_score(state)  # Đánh giá tấn công
+        score -= self.attack_score(state)* 0.5  # Đánh giá tấn công
         # print(score)
-        score += self.defense_score(state)  # Đánh giá phòng thủ
+        score += self.defense_score(state)* 0.3  # Đánh giá phòng thủ
         # print(score)
-        score -= self.influence_score(state)  # Đánh giá ảnh hưởng
+        score -= self.influence_score(state)* 0.2  # Đánh giá ảnh hưởng
         # print(score)
-        score += self.blocking_score(state)  # Đánh giá chặn đường đi của đối thủ
+        score += self.blocking_score(state)* 0.4  # Đánh giá chặn đường đi của đối thủ
+        score += self.Euler_number(state.get_board(),self.color)-self.Euler_number(state.get_board(),self.op_color) # Create an eye 
         # print(score)
         # print('***')
         return score
@@ -84,22 +85,22 @@ class AlphaBetaPruningAgent(ABCAgent):
 
     def attack_score(self, state):
         # Đánh giá tấn công bằng cách đếm số lượng quân cờ tấn công đối thủ
-        attack_score = self.count_stones(state.get_board(), self.op_color) * 0.5
+        attack_score = self.count_stones(state.get_board(), self.op_color) 
         return attack_score
 
     def defense_score(self, state):
         # Đánh giá phòng thủ bằng cách đếm số lượng quân cờ phòng thủ của người chơi
-        defense_score = self.count_stones(state.get_board(), self.color) * 0.3
+        defense_score = self.count_stones(state.get_board(), self.color) 
         return defense_score
 
     def influence_score(self, state):
         # Đánh giá ảnh hưởng bằng cách đếm số lượng quân cờ gần biên giới của người chơi
-        influence_score = self.count_boundary_stones(state.get_board(), self.color) * 0.2
+        influence_score = self.count_boundary_stones(state.get_board(), self.color) 
         return influence_score
 
     def blocking_score(self, state):
         # Đánh giá chặn đường đi của đối thủ bằng cách đếm số lượng đường đi bị chặn
-        blocking_score = self.count_blocked_paths(state.get_board(), self.op_color) * 0.4
+        blocking_score = self.count_blocked_paths(state.get_board(), self.op_color)
         return blocking_score
 
     def count_stones(self, board, player):
@@ -134,6 +135,37 @@ class AlphaBetaPruningAgent(ABCAgent):
                         if board[ni][nj] == stone.EMPTY:
                             count += 1
         return count
+    def Euler_number(self, board, player):
+        # heuristic count to create an eye
+        Q1=0;
+        Q3=0;
+        Qd=0
+        for i in range(8):
+            for j in range(8):
+                #Q1
+                if board[i][j] == player and board[i+1][j] == stone.EMPTY and board[i][j+1] == stone.EMPTY and board[i+1][j+1] == stone.EMPTY:
+                    Q1+=1
+                if board[i][j] == stone.EMPTY and board[i+1][j] == player and board[i][j+1] == stone.EMPTY and board[i+1][j+1] == stone.EMPTY:
+                    Q1+=1
+                if board[i][j] == stone.EMPTY and board[i+1][j] == stone.EMPTY and board[i][j+1] == player and board[i+1][j+1] == stone.EMPTY:
+                    Q1+=1
+                if board[i][j] == stone.EMPTY and board[i+1][j] == stone.EMPTY and board[i][j+1] == stone.EMPTY and board[i+1][j+1] == player:
+                    Q1+=1
+                #Q3
+                if board[i][j] == stone.EMPTY and board[i+1][j] == player and board[i][j+1] == player and board[i+1][j+1] == player:
+                    Q3+=1
+                if board[i][j] == player and board[i+1][j] == stone.EMPTY and board[i][j+1] == player and board[i+1][j+1] == player:
+                    Q3+=1
+                if board[i][j] == player and board[i+1][j] == player and board[i][j+1] == stone.EMPTY and board[i+1][j+1] == player:
+                    Q3+=1
+                if board[i][j] == player and board[i+1][j] == player and board[i][j+1] == player and board[i+1][j+1] == stone.EMPTY:
+                    Q3+=1
+                #Qd
+                if board[i][j] == player and board[i+1][j] == stone.EMPTY and board[i][j+1] == stone.EMPTY and board[i+1][j+1] == player:
+                    Qd+=1
+                if board[i][j] == stone.EMPTY and board[i+1][j] == player and board[i][j+1] == player and board[i+1][j+1] == stone.EMPTY:
+                    Qd+=1
+        return (10/self.game.num_moves*(Q1-Q3)-abs(35-self.game.num_moves)*2*Qd)/4
 
     def min_value(self, depth: int, alpha: float, beta: float):
         if depth == 0 or self.game.is_over():
@@ -183,6 +215,7 @@ class AlphaBetaPruningAgent(ABCAgent):
                 new_sc = self.game.score()[self.color] - self.game.score()[self.op_color]
                 self.game.step_up()
                 self.game.step_up()
+            
             if self.game.num_moves>=60:
                 if new_sc - init_sc >= 0:
                     return None, self.evaluate_board(self.game)
